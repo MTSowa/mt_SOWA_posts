3581bab1210970ee6b5e78e100d81cce92573d92
MTSowa <sowaf57@gmail.com>
2021-09-10 13:17:17 +0000

    Add Posts functionality

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const ejs = require("ejs");
+// const { title } = require("process");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -15,14 +16,17 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
-// %%%%%%%%%%%%%%%%%%%%%%%%       VARIABLES   %%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+// %%%%%%%%%%%%%%%%%%%%%%%%    GL VARIABLES   %%%%%%%%%%%%%%%%%%%%%%%%%%%
+let Posts = [];
 
 
 // %%%%%%%%%%%%%%%%%%%%%%%%    GET REQUESTS   %%%%%%%%%%%%%%%%%%%%%%%%%%%
 // home page
 app.get('/',(req,res)=>{
-  res.render('home',{homeStartingContent:homeStartingContent})
+  res.render('home',{
+    homeStartingContent:homeStartingContent,
+    Posts:Posts
+  })
 })
 
 // about page
@@ -41,7 +45,17 @@ app.get('/compose',(req,res)=>{
 
 // %%%%%%%%%%%%%%%%%%%%%%%%   POST REQUESTS   %%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
+// compose post page
+app.post('/compose',(req,res)=>{
+  let title = req.body.composeText_title;
+  let content = req.body.composeText_content;
+  let post = {
+    title:title,
+    content:content
+  }
+  Posts = [...Posts,post];
+  res.redirect('/');
+})
 
 
 
